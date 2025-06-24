45be4cf42cc3a184ed2541ecc448d3b56d464383
unknown <amoshsapkota@gmail.com>
2025-06-24 15:39:48 -0400

    Changed app_service to kubernetes.tf

--- automation/iac/app_svc/terraform/kubernetes.tf
+++ automation/iac/app_svc/terraform/kubernetes.tf
@@ -4,10 +4,15 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   dns_prefix          = local.aks_dns_prefix
+  
   default_node_pool {
     name       = "default"
     node_count = var.node_count
     vm_size    = var.node_vm_size
     max_pods   = 30
+  }
+
+  identity {
+    type = "SystemAssigned"  # REQUIRED
   }
 }
